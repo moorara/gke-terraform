a0033f4d0851e82ba01552e0b89eea5382ca24f7
Milad Irannejad <moorara@gmail.com>
2019-11-28 13:05:30 -0500

    Fix kubectl connectivity issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,9 +8,22 @@ terraform {
 }
 
 # https://www.terraform.io/docs/providers/google
-# https://www.terraform.io/docs/providers/google/getting_started.html
-# https://www.terraform.io/docs/configuration/providers.html#version-provider-versions
+# https://www.terraform.io/docs/providers/google/guides/provider_reference.html
+# https://www.terraform.io/docs/providers/google/guides/provider_versions.html
+# https://www.terraform.io/docs/configuration/providers.html#provider-versions
 provider "google" {
+  # Equivalent to ">= 2.20.0, < 2.0.0"
+  version     = "~> 2.20"
+  credentials = file("account.json")
+  project     = var.project
+  region      = var.region
+}
+
+# https://www.terraform.io/docs/providers/google
+# https://www.terraform.io/docs/providers/google/guides/provider_reference.html
+# https://www.terraform.io/docs/providers/google/guides/provider_versions.html
+# https://www.terraform.io/docs/configuration/providers.html#provider-versions
+provider "google-beta" {
   # Equivalent to ">= 2.20.0, < 2.0.0"
   version     = "~> 2.20"
   credentials = file("account.json")
